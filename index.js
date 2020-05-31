139830723644d685ea2cf9bdeab0e7998459f971
Nick Tang <tangnick@live.com>
2020-05-31 19:47:19 -0400

    working connection to our db

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path')
 
+const config = require('./config')()
+const { Pool } = require('pg');
+const pool = new Pool({
+    user: config.user,
+    host: config.host,
+    database: config.database,
+    password: config.password,
+    port: config.port
+});
+
+pool.query('SELECT * FROM USERS', (err, res) =>{
+    console.log(err,res)
+    pool.end()  
+})
+
 // configure where express reads CSS and JS files
 app.use(express.static(path.join(__dirname, 'public')));
 // configure view engine
