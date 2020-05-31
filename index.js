ef132877d36e22f767bd934ff265b6a98d92090d
Nick Tang <tangnick@live.com>
2020-05-31 19:01:07 -0400

    specify the routes

--- index.js
+++ index.js
@@ -8,10 +8,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 // configure view engine
 app.set('view engine', 'ejs')
 
-app.get('/', function (req, res) {
-    res.render('index')
-})
+app.get(['/', '/login'], function (req, res) {
+    res.render('login');
+});
 
+app.get('/logout', function(req, res) {
+    res.redirect('/login');
+});
 
+app.get('/draw', function(req, res) {
+    res.render('draw');
+});
+
+app.get('/home', function(req, res) {
+    res.render('home');
+});
+
+// redirect all other routes to login
+app.use(function(req, res) {
+    res.redirect('/login');
+});
 
 app.listen(port, () => console.log(`listening on port ${port}`));
